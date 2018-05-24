932b5e7d4d9ade1fb5a3311bf4f422fbe2b134ba
Vera Bruett <Vera.Bruett@heuboe.de>
2018-05-24 16:14:50 +0200

    layout

--- main.cpp
+++ main.cpp
@@ -11,14 +11,13 @@
 using namespace std;
 
 const int citizenNumber = 400;
-//string partyArray[citizenNumber];
 int arraySize = citizenNumber - 1;
 int height = 10;
 int width = 40;
 
 class Person{
 public:
-  int party;
+    int party;
 };
 Person partyArray[citizenNumber];
 
@@ -41,6 +40,77 @@ bool isNotFinished(){
     return false;
 }
 
+void output(){
+    system("cls");
+    for (int index = 0; index < arraySize; index++) {
+        if(partyArray[index].party == 1){
+            cout<<"A";
+        }
+        else{
+            cout<<"-";
+        }
+    }
+}
+
+void compare(int party1, int party2, int indexCitizen, int neighbour){
+    if(party1 != party2){
+        int i = 1 + rand() % 2;
+        if(i == 1){ //Wenn der anderer Buerger ueberzeugt wurde
+            partyArray[neighbour].party = party1;
+        }
+        else{
+            partyArray[indexCitizen].party = party2;
+        }
+        output();
+
+        //VOID WINAPI Sleep( DWORD dwMilliseconds );
+        //Sleep(100);
+    }
+
+}
+
+int getNeighbour(int party2, int indexCitizen, int neighbour){
+    switch(neighbour){
+    case 1: {
+        if(indexCitizen < width){
+            neighbour = indexCitizen + (citizenNumber - width);
+        }else{
+            neighbour = indexCitizen - width;
+        }
+        party2 = partyArray[neighbour].party;
+        break;
+    }
+    case 2:{
+        if(indexCitizen == arraySize){
+            neighbour = 0;
+        }else{
+            neighbour = indexCitizen + 1;
+        }
+        party2 = partyArray[neighbour].party;
+        break;
+    }
+    case 3:{
+        if(indexCitizen >= (citizenNumber - width)){
+            neighbour = indexCitizen - (citizenNumber - width);
+        }else{
+            neighbour = indexCitizen + width;
+        }
+        party2 = partyArray[neighbour].party;
+        break;
+    }
+    case 4: {
+        if(indexCitizen  == 0){
+            neighbour = arraySize;
+        }else{
+            neighbour = indexCitizen - 1;
+        }
+        party2 = partyArray[neighbour].party;
+        break;
+    }
+    }
+    return party2;
+}
+
 int main()
 {
     srand((int)time(0));
@@ -53,70 +123,9 @@ int main()
         int indexCitizen = rand() % citizenNumber;
         int party1 = partyArray[indexCitizen].party;
         int neighbour = 1 + rand() % 4;
-        int party2;
+        int party2 = getNeighbour(party2, indexCitizen, neighbour);
+        compare(party1, party2, indexCitizen, neighbour);
 
-        switch(neighbour){
-        case 1: {
-            if(indexCitizen < width){
-                neighbour = indexCitizen + (citizenNumber - width);
-            }else{
-                neighbour = indexCitizen - width;
-            }
-            party2 = partyArray[neighbour].party;
-            break;
-        }
-        case 2:{
-            if(indexCitizen == arraySize){
-                neighbour = 0;
-            }else{
-                neighbour = indexCitizen + 1;
-            }
-            party2 = partyArray[neighbour].party;
-            break;
-        }
-        case 3:{
-            if(indexCitizen >= (citizenNumber - width)){
-                neighbour = indexCitizen - (citizenNumber - width);
-            }else{
-                neighbour = indexCitizen + width;
-            }
-            party2 = partyArray[neighbour].party;
-            break;
-        }
-        case 4: {
-            if(indexCitizen  == 0){
-                neighbour = arraySize;
-            }else{
-                neighbour = indexCitizen - 1;
-            }
-            party2 = partyArray[neighbour].party;
-            break;
-        }
-        }
-
-        if(party1 != party2){
-            int i = 1 + rand() % 2;
-            if(i == 1){
-                partyArray[neighbour].party = party1;
-            }
-            else{
-                partyArray[indexCitizen].party = party1;
-            }
-            system("cls");
-            for (int index = 0; index < arraySize; index++) {
-                if(partyArray[index].party == 1){
-                    cout<<"A";
-                }
-                else{
-                    cout<<"-";
-                }
-            }
-            //VOID WINAPI Sleep( DWORD dwMilliseconds );
-            //Sleep(100);
-        }
-
-
-
-}while(isNotFinished());
-return 0;
+    }while(isNotFinished());
+    return 0;
 }
